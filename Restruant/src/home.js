2a264beead7a3db6ee78934ff5888c9d3b352eae
Alfiya-Anjum <125040235+Alfiya-Anjum@users.noreply.github.com>
2025-08-19 02:34:31 +0300

    complete home page

--- Restruant/src/home.js
+++ Restruant/src/home.js
@@ -1,11 +1,6 @@
 export default function loadHome() {
   const content = document.getElementById("content");
 
-  const headline = document.createElement("h1");
-  headline.textContent = "Welcome to My Restaurant 🍴";
-
-  const desc = document.createElement("p");
-  desc.textContent = "The best food in town, made with love.";
 
   content.appendChild(headline);
   content.appendChild(desc);
