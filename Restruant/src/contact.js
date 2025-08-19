75c9026da373c5615dd93d226854090fbd3d519d
Alfiya-Anjum <125040235+Alfiya-Anjum@users.noreply.github.com>
2025-08-20 01:44:00 +0300

    contact page complete

--- Restruant/src/contact.js
+++ Restruant/src/contact.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 export default function loadContact() {
   const content = document.getElementById("content");
   content.innerHTML = "";
@@ -12,10 +14,16 @@ export default function loadContact() {
       </nav>
     </header>
 
+
+    <div class="contact">
     <h2>Contact Us</h2>
     <p>📍 123 Flavor Street, Foodtown</p>
     <p>📞 (123) 456-7890</p>
     <p>📧 hello@restaurant.com</p>
+
+    <p>🕒 Open Daily: 10 AM - 10 PM</p>
+
+    </div>
   `;
 
   content.appendChild(contactPage);
